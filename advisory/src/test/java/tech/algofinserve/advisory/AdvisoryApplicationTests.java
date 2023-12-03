2cdb2947ddf0ea1926041a50aa8087b73d766fec
snehalshah143 <96965379+snehalshah143@users.noreply.github.com>
2023-12-03 21:42:43 +0530

    Initial commit

---
+++ advisory/src/test/java/tech/algofinserve/advisory/AdvisoryApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.algofinserve.advisory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvisoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
